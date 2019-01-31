31261bfae1d8882ae78a19f7a332648258146a94
dimazap <dima.zap@gmail.com>
2019-01-31 17:38:42 +0100

    cleaner utils.py

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,13 @@ from pprint import pprint
 import json
 import requests
 
-structure = {'slice[]': 'offer', 'filter[advertiser]': '5b2a22d373bdda49008b4604', 'filter[date_to]': '2018-10-02', 'filter[date_from]': '2018-10-01', 'API-key': '37796ce6525a581c56a493ccd39402eadca187de'}
+structure = {
+    'slice[]': 'offer', 
+    #'filter[advertiser]': '5b2a22d373bdda49008b4604', 
+    'filter[date_to]': '2018-10-02', 
+    'filter[date_from]': '2018-10-01', 
+    'API-key': '37796ce6525a581c56a493ccd39402eadca187de'
+    }
 
 r = requests.get('https://api.dzdata.affise.com/3.0/stats/custom', verify=False, params=structure)
 
@@ -12,7 +18,7 @@ pprint(data)
 
 elems = data['stats']
 
-#offers = [
+offers = [
     {
         'id': elem['slice']['offer']['id'],
         'title': elem['slice']['offer']['title'],
